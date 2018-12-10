17c710079bf91ca2c5b66f4a69e9fd8c23f3ef3e
Kip Yin <kipyyin@gmail.com>
2018-12-11 01:52:01 +0800

    Add moves and gender properties to Pokemon

--- pokemaster/pokemon.py
+++ pokemaster/pokemon.py
@@ -3,32 +3,49 @@
     Basic Pokémon API
 
 """
+import datetime
 from random import randint
 from typing import *
 
 from construct import Struct, Int8ul, Int32ul, Padding
 from pokedex.db import tables as tb, util, connect
 
-from pokemaster import session
+from pokemaster.session import get_session
+
+
+def lcrng(seed: int):
+    """The Linear Congruential random number generator in Gen III & IV.
+
+    The initial seed in Emerald is always 0. See:
+    https://bulbapedia.bulbagarden.net/wiki/Pseudorandom_number_generation_in_Pokémon
+    """
+    while True:
+        seed = 0x41C64E6D * seed + 0x00006073
+        seed &= 0xFFFFFFFF
+        yield seed >> 16
 
 
 class Pokemon:
     """A Pokémon"""
 
-    SESSION: ClassVar = session.initialize_database()
+    SESSION: ClassVar = get_session()
 
     def __init__(self, identity: Union[str, int], level=None):
 
         self._session = Pokemon.SESSION
 
         if isinstance(identity, str):
-            self._pokemon = util.get(_session, tb.Pokemon, identifier=identity)
+            self._pokemon = util.get(
+                self._session, tb.Pokemon, identifier=identity
+            )
             self._species = util.get(
-                _session, tb.PokemonSpecies, identifier=identity
+                self._session, tb.PokemonSpecies, identifier=identity
             )
         elif isinstance(identity, int):
-            self._pokemon = util.get(_session, tb.Pokemon, id=identity)
-            self._species = util.get(_session, tb.PokemonSpecies, id=identity)
+            self._pokemon = util.get(self._session, tb.Pokemon, id=identity)
+            self._species = util.get(
+                self._session, tb.PokemonSpecies, id=identity
+            )
         else:
             raise TypeError(
                 f'`identity` must be a str or an int, not {type(identity)}'
@@ -41,26 +58,13 @@ class Pokemon:
         self.name = self._pokemon.name
         self.level = level or 5
 
-        # A Pokémon's gender is determined by the last byte of its PID.
-        # The tb.Species.gender_rate is the probablity of a Pokémon being
-        # a female, in eighth. -1 for genderless.
-        # So to convert between these two intepretations, we need to
-        # translate the probability into binary, and then compare the
-        # last byte of the PID with the binary probability. If the former
-        # is greater, then this Pokémon is male.
-        self._gender_base_value = self._species.gender_rate
-
         self._all_moves = (
             self._session.query(tb.Move)
             .join(tb.PokemonMove, tb.Move.id == tb.PokemonMove.move_id)
             .filter(tb.PokemonMove.pokemon_id == self.id)
             .filter(tb.PokemonMove.version_group_id == 6)  # Hack
-            .filter(tb.PokemonMove.pokemon_move_method_id.in_([1, 2]))
-            .filter(tb.PokemonMove.level <= self.level)
-            .order_by(tb.PokemonMove.level.desc())
-            .order_by(tb.PokemonMove.order)
         )
-        self.moves = self._all_moves.limit(4).all()
+        self._moves = []
 
         # self._all_abilities = (
         #     _session
@@ -78,3 +82,39 @@ class Pokemon:
         #     .filter(tb.PokemonAbility.is_hidden == 1)
         #     .one_or_none()
         # )
+
+    @property
+    def gender(self):
+        """Return the gender of a pokemon.
+
+        A Pokémon's gender is determined by the last byte of its PID,
+        and its gender rate. The :meth:`tb.Species.gender_rate` is the
+        probablity of a Pokémon being a female, in eighths. -1 means
+        genderless.
+        """
+        if self._species.gender_rate == -1:  # genderless
+            return 3
+
+        threshold = 0xFF * self._species.gender_rate // 8
+        gender_struct = Struct('value' / Int8ul, Padding(3))
+
+        if gender_struct.parse(self.pid).value >= threshold:
+            return 2  # male
+        else:
+            return 1  # female
+
+    @property
+    def moves(self):
+        """List of this Pokémon's current moves."""
+        if self._moves:
+            return self._moves
+        return (
+            self._all_moves.filter(
+                tb.PokemonMove.pokemon_move_method_id.in_([1, 2])
+            )
+            .filter(tb.PokemonMove.level <= self.level)
+            .order_by(tb.PokemonMove.level.desc())
+            .order_by(tb.PokemonMove.order)
+            .limit(4)
+            .all()
+        )
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -4,14 +4,23 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../..')))
 
+from construct import Int32ul
+
 from pokemaster.pokemon import Pokemon
 
 
 class TestPokemon:
-    def test_pokemon_methods(self):
+    def test_pokemon_properties(self):
 
         bulbasaur = Pokemon(1)
         assert bulbasaur.id == 1
         assert bulbasaur.identifier == 'bulbasaur'
         assert bulbasaur.name == 'Bulbasaur'
         assert list(map(lambda x: x.id, bulbasaur.moves)) == [45, 33, 80, 113]
+
+    def test_pokemon_gender(self):
+        bulbasaur = Pokemon(1)
+        bulbasaur.pid = Int32ul.build(0xC0FFEE)
+        assert bulbasaur.gender == 2  # male
+        nidoran_f = Pokemon('nidoran-f')
+        assert nidoran_f.gender == 1
