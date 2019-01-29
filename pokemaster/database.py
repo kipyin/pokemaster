8501fdd92c093a57c0b4ccef750344b828328350
Kip Yin <kipyyin@gmail.com>
2019-01-29 22:55:48 +0800

    Add query functions for Machines and Move Pools

--- pokemaster/database.py
+++ pokemaster/database.py
@@ -1,35 +1,36 @@
-"""Query helper functions.
+"""Query helpers.
 
 Make simple queries in a breeze anywhere, any time.
 Note that tables should not be passed as arguments! That'll defeat
 the purpose of this module.
 """
 import warnings
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Iterable
 
 import pokedex
 import pokedex.db
 import pokedex.db.load
 import pokedex.defaults
+import sqlalchemy.exc
 import sqlalchemy.orm.session
-from pokedex.db import tables as tb
-from sqlalchemy.exc import OperationalError
+
+from pokemaster.prng import PRNG
 
 
-def get_session(engine_uri: str = None) -> sqlalchemy.orm.session.Session:
+def get_session(database_uri: str = None) -> sqlalchemy.orm.session.Session:
     """Connect to a database with the given ``engine_uri``.
 
-    :param engine_uri: The uri of the database. The default uri set by
+    :param database_uri: The uri of the database. The default uri set by
     :mod:`pokedex.defaults` will be used if not specified..
     :return: A ``sqlalchemy.orm.session.Session``.
     """
 
-    engine_uri = engine_uri or pokedex.defaults.get_default_db_uri()
+    database_uri = database_uri or pokedex.defaults.get_default_db_uri()
 
     try:
-        session = pokedex.db.connect(engine_uri)
-    except OperationalError:
-        warnings.warn(f'Wrong database uri: {engine_uri}.')
+        session = pokedex.db.connect(database_uri)
+    except sqlalchemy.exc.OperationalError:
+        warnings.warn(f'Wrong database uri: {database_uri}.')
         warnings.warn(
             'Connecting to the default database: '
             f'{pokedex.defaults.get_default_db_uri()}.'
@@ -40,7 +41,7 @@ def get_session(engine_uri: str = None) -> sqlalchemy.orm.session.Session:
         # Empty database
         warnings.warn('Initializing database')
         pokedex.db.load.load(session, drop_tables=True, safe=False)
-        session = pokedex.db.connect(engine_uri)
+        session = pokedex.db.connect(database_uri)
 
     return session
 
@@ -48,15 +49,29 @@ def get_session(engine_uri: str = None) -> sqlalchemy.orm.session.Session:
 SESSION = get_session()
 
 
-def bind_session(session):
+def set_session(session):
+    """Bind a session."""
     global SESSION
     SESSION = session
 
 
+def _check_completeness(
+    *args, msg='Must specify at least one value.'
+) -> Optional[bool]:
+    """Return True if at least one argument in args is not None."""
+    for arg in args:
+        if isinstance(arg, Iterable):
+            return _check_completeness(*arg)
+        if arg is not None:
+            return True
+    else:
+        raise ValueError(msg)
+
+
 def get_pokemon(
     national_id: int = None, species: str = None, form: str = None
-) -> tb.Pokemon:
-    """Query a ``pokedex.db.tables.Pokemon`` row.
+) -> pokedex.db.tables.Pokemon:
+    """Make a query from ``pokedex.db.tables.Pokemon``.
 
     :param national_id: The National Pokédex ID.
     :param species: Pokémon's species, the standard 386, 493, etc.
@@ -64,75 +79,90 @@ def get_pokemon(
         'unown', 'attack' for 'deoxys', etc.
     :return: a ``pokedex.db.tables.Pokemon`` row.
     """
-    if national_id is None and species is None:
-        raise ValueError('Gimme something to search!')
-    query = SESSION.query(tb.Pokemon).join(tb.PokemonForm, tb.PokemonSpecies)
+    _check_completeness(national_id, species)
+    query: sqlalchemy.orm.query.Query = (
+        SESSION.query(pokedex.db.tables.Pokemon)
+        .join(pokedex.db.tables.PokemonForm)
+        .join(pokedex.db.tables.PokemonSpecies)
+    )
     if national_id is not None:
-        query = query.filter(tb.PokemonSpecies.id == national_id)
+        query = query.filter(pokedex.db.tables.PokemonSpecies.id == national_id)
     if species is not None:
-        query = query.filter(tb.PokemonSpecies.identifier == species)
+        query = query.filter(
+            pokedex.db.tables.PokemonSpecies.identifier == species
+        )
     if form is not None:
-        query = query.filter(tb.PokemonForm.form_identifier == form)
+        query = query.filter(
+            pokedex.db.tables.PokemonForm.form_identifier == form
+        )
     else:
-        query = query.filter(tb.Pokemon.is_default == 1)
+        query = query.filter(pokedex.db.tables.Pokemon.is_default == 1)
     return query.one()
 
 
-def pokemon_species(
-    national_id: int = None, identifier: str = None, name: str = None, **kwargs
-) -> tb.PokemonSpecies:
-    """Get a ``PokemonSpecies`` table instance."""
-    if national_id is None and identifier is None and name is None:
-        raise ValueError('Gimme something to search!')
-    if national_id is not None and isinstance(national_id, int):
-        kwargs['id'] = national_id
-    if identifier is not None and isinstance(identifier, str):
-        kwargs['identifier'] = identifier
-    if name is not None and isinstance(name, str):
-        kwargs['name'] = name
-    return SESSION.query(tb.PokemonSpecies).filter_by(**kwargs).one()
-
-
-def get_experience(
-    species_identifier: str = None,
+def get_experience_table(
+    national_id: int = None,
+    species: str = None,
+    form: str = None,
     growth_rate_id: int = None,
-    level: int = None,
-    exp: int = None,
-) -> tb.Experience:
-    """Look up a Pokémon's experience at various levels."""
+) -> sqlalchemy.orm.query.Query:
+    """Get an experience table specific to the species.
 
+    :param form:
+    :param national_id: The National Pokédex ID.
+    :param species: The Pokémon's species.
+    :param growth_rate_id: The Pokémon's growth rate.
+    :return: sqlalchemy.orm.query.Query
+    """
+    # _check_completeness([national_id, species], growth_rate_id)
     if growth_rate_id is not None:
         # If `growth_rate_id` is given, then ignore `species_identifier`.
         conditions = {'growth_rate_id': growth_rate_id}
-    elif species_identifier is not None:
-        species = get_pokemon(identifier=species_identifier)
-        conditions = {'growth_rate_id': species.growth_rate_id}
+    elif species is not None or national_id is not None:
+        pokemon = get_pokemon(
+            species=species, national_id=national_id, form=form
+        )
+        conditions = {'growth_rate_id': pokemon.species.growth_rate_id}
     else:
         raise ValueError(
             'Must specify either the species or the growth rate ID.'
         )
+    return SESSION.query(pokedex.db.tables.Experience).filter_by(**conditions)
+
+
+def get_experience(
+    national_id: int = None,
+    species: str = None,
+    growth_rate_id: int = None,
+    level: int = None,
+    exp: int = None,
+) -> pokedex.db.tables.Experience:
+    """Look up a Pokémon's experience at various levels."""
 
     if level is None and exp is None:
         raise ValueError('Gimme something to look up!')
 
+    query = get_experience_table(
+        national_id=national_id, species=species, growth_rate_id=growth_rate_id
+    )
     if level is not None:
-        conditions['level'] = level
-    _query = SESSION.query(tb.Experience).filter_by(**conditions)
-
+        query = query.filter_by(level=level)
     if exp is not None:
-        _query = (
-            _query.filter(tb.Experience.experience <= exp)
-            .order_by(tb.Experience.experience)
-            .desc()
-        )
-    return _query.one()
+        query = query.filter(
+            pokedex.db.tables.Experience.experience <= exp
+        ).order_by(pokedex.db.tables.Experience.experience.desc())
+    result = query.first()
+    if result is None:
+        raise ValueError(f'Inconsistent data.')
+    else:
+        return result
 
 
 def wild_pokemon_held_item(
-    prng: "PRNG", national_id: int, compound_eyes: bool
-) -> tb.Item:
+    prng: PRNG, national_id: int, compound_eyes: bool
+) -> Optional[pokedex.db.tables.Item]:
     """Determine the held item of a wild Pokémon."""
-    pokemon_ = get_pokemon(id=national_id)
+    pokemon_ = get_pokemon(national_id=national_id)
 
     held_item_chance = prng() / 0xFFFF
 
@@ -154,26 +184,36 @@ def wild_pokemon_held_item(
         return None
 
 
-def wild_pokemon_moves(pokemon: tb.Pokemon, level: int) -> Tuple[tb.Move]:
+def get_pokemon_default_moves(
+    level: int,
+    national_id: int = None,
+    species: str = None,
+    form: str = None,
+    version_group: str = 'emerald',
+) -> Tuple[pokedex.db.tables.Move]:
     """Determine the moves of a wild Pokémon."""
-    pokemon_moves: List[tb.PokemonMove] = (
-        SESSION.query(tb.PokemonMove)
-        .join(tb.PokemonMoveMethod)
-        .join(tb.VersionGroup)
+    pokemon = get_pokemon(national_id, species, form)
+    pokemon_moves: List[pokedex.db.tables.PokemonMove] = (
+        SESSION.query(pokedex.db.tables.PokemonMove)
+        .join(pokedex.db.tables.PokemonMoveMethod)
+        .join(pokedex.db.tables.VersionGroup)
         .filter(
-            tb.PokemonMove.pokemon_id == pokemon.id,
-            tb.PokemonMoveMethod.identifier == 'level-up',
-            tb.PokemonMove.level <= level,
-            tb.VersionGroup.identifier == 'emerald',
+            pokedex.db.tables.PokemonMove.pokemon_id == pokemon.id,
+            pokedex.db.tables.PokemonMoveMethod.identifier == 'level-up',
+            pokedex.db.tables.PokemonMove.level <= level,
+            pokedex.db.tables.VersionGroup.identifier == version_group,
         )
-        .order_by(tb.PokemonMove.level.desc(), tb.PokemonMove.order)
+        .order_by(pokedex.db.tables.PokemonMove.level.desc())
+        .order_by(pokedex.db.tables.PokemonMove.order)
         .limit(4)
         .all()
     )
     return tuple(map(lambda x: x.move, pokemon_moves))
 
 
-def nature(personality: int = None, identifier: str = None) -> tb.Nature:
+def get_nature(
+    personality: int = None, identifier: str = None
+) -> pokedex.db.tables.Nature:
     """Determine a Pokémon's nature from its personality value."""
     conditions = {}
     if personality is None and identifier is None:
@@ -182,22 +222,24 @@ def nature(personality: int = None, identifier: str = None) -> tb.Nature:
         conditions['game_index'] = personality % 25
     if identifier is not None:
         conditions['identifier'] = identifier
-    return SESSION.query(tb.Nature).filter_by(**conditions).one()
+    return SESSION.query(pokedex.db.tables.Nature).filter_by(**conditions).one()
 
 
-def ability(species: str, personality: int) -> tb.Ability:
+def get_ability(species: str, personality: int) -> pokedex.db.tables.Ability:
     """Not quite a query, but definitely hides the mess.
 
     If a Pokémon only have one ability, then that'll be its
     ability straight away; if it has more than one ability,
     then the last bit of `personality` comes to play.
     """
-    pokemon_ = get_pokemon(identifier=species)
+    pokemon_ = get_pokemon(species=species)
     abilities = pokemon_.abilities
     return abilities[min(len(abilities) - 1, personality % 2)]
 
 
-def pokemon_gender(gender_rate: int, personality: int) -> tb.Gender:
+def get_pokemon_gender(
+    gender_rate: int, personality: int
+) -> pokedex.db.tables.Gender:
     """Determine a Pokémon's gender by its gender rate and personality."""
     if gender_rate == -1:
         gender = 'genderless'
@@ -213,4 +255,55 @@ def pokemon_gender(gender_rate: int, personality: int) -> tb.Gender:
             gender = 'male'
         else:
             gender = 'female'
-    return SESSION.query(tb.Gender).filter_by(identifier=gender).one()
+    return (
+        SESSION.query(pokedex.db.tables.Gender)
+        .filter_by(identifier=gender)
+        .one()
+    )
+
+
+def get_move(move: str = None, move_id: int = None) -> pokedex.db.tables.Move:
+    """"""
+    return (
+        SESSION.query(pokedex.db.tables.Move).filter_by(
+            identifier=move, id=move_id
+        )
+    ).one()
+
+
+def get_machine(machine_number: int) -> pokedex.db.tables.Machine:
+    """Get a TM or HM by the machine number."""
+    return (
+        SESSION.query(pokedex.db.tables.Machine)
+        .join(pokedex.db.tables.VersionGroup)
+        .filter(
+            pokedex.db.tables.Machine.machine_number == machine_number,
+            pokedex.db.tables.VersionGroup.identifier == 'emerald',
+        )
+    ).one()
+
+
+def get_move_pool(
+    species: str, move_method: str = None
+) -> List[pokedex.db.tables.PokemonMove]:
+    """Get a pool of moves that a Pokémon can learn via a specific method."""
+    query = (
+        SESSION.query(pokedex.db.tables.PokemonMove)
+        .join(pokedex.db.tables.PokemonMoveMethod)
+        .join(pokedex.db.tables.Pokemon)
+        .join(pokedex.db.tables.PokemonSpecies)
+        .filter(
+            pokedex.db.tables.PokemonSpecies.identifier == species,
+            pokedex.db.tables.VersionGroup.identifier == 'emerald',
+        )
+    )
+    if move_method is not None:
+        query = query.filter(
+            pokedex.db.tables.PokemonMoveMethod.identifier == move_method
+        )
+    return query.all()
+
+
+if __name__ == '__main__':
+    p = get_pokemon(species='deoxys')
+    print(p.species.identifier)
