4c4874c0270995fc5f3ff6f64d603fd01a435244
Kip Yin <kipyyin@gmail.com>
2019-02-02 00:09:20 +0800

    Add tests for `pokemaster.database.get_pokemon()`

--- tests/test_database.py
+++ tests/test_database.py
@@ -20,3 +20,35 @@ def test_get_machine():
 def test_get_move_pool():
     assert database.get_move_pool(species='eevee', move_method='level-up')
     assert len(database.get_move_pool(species='eevee')) != 0
+
+
+def test_get_pokemon_by_national_id():
+    """Given the national ID, a single Pokémon should be returned."""
+    bulbasaur = database.get_pokemon(national_id=1)
+    assert bulbasaur.identifier == 'bulbasaur'
+
+
+def test_get_pokemon_with_different_forms_by_national_id_():
+    """Deoxys has 3 forms. `get_pokemon` should only return the default
+    form.
+    """
+    deoxys_normal = database.get_pokemon(national_id=386)
+    assert deoxys_normal.identifier == 'deoxys-normal'
+
+
+def test_get_pokemon_by_species():
+    """Get a Pokémon by its species."""
+    bulbasaur = database.get_pokemon(species='bulbasaur')
+    assert bulbasaur.identifier == 'bulbasaur'
+
+
+def test_get_pokemon_by_national_id_and_form():
+    """Get a Pokémon by its National Pokédex ID and the form."""
+    unown_x = database.get_pokemon(national_id=201, form='x')
+    assert unown_x.identifier == 'unown'
+
+
+def test_get_pokemon_by_species_and_form():
+    """Get a Pokémon by its species and its form."""
+    castform_rainy = database.get_pokemon(species='castform', form='rainy')
+    assert castform_rainy.identifier == 'castform-rainy'
