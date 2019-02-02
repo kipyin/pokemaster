6ac990a2bf8af21e4b74f1f26ac270ea1889fc38
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-02-03 01:05:55 +0800

    Remove old tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,9 +1,6 @@
-import pytest
-from attr.exceptions import FrozenInstanceError
-from hypothesis import given
-from hypothesis.strategies import integers, lists
-
-from pokemaster.exceptions import MaxStatExceededError
+"""
+Tests for ``pokemaster.stats``.
+"""
 from pokemaster.stats import Stats
 
 
@@ -26,41 +23,3 @@ def test_stats_multiplication():
     )
     assert Stats(1, 2, 3, 4, 5, 6) * 2 == Stats(2, 4, 6, 8, 10, 12)
     assert 2 * Stats(1, 2, 3, 4, 5, 6) == Stats(2, 4, 6, 8, 10, 12)
-
-
-@pytest.mark.skip
-@given(lists(elements=integers(min_value=33), min_size=6, max_size=6))
-def test_error_when_iv_exceeds_32(iv_values):
-    with pytest.raises(MaxStatExceededError):
-        Stats(*iv_values)
-
-
-@pytest.mark.skip
-@given(
-    lists(elements=integers(min_value=0, max_value=32), min_size=6, max_size=6)
-)
-def test_iv_immutability(iv_values):
-    """Once the IV is set, it should not be changed."""
-    iv = IV(*iv_values)
-    for stat in [
-        'hp',
-        'attack',
-        'defense',
-        'special_attack',
-        'special_defense',
-        'speed',
-    ]:
-        with pytest.raises(FrozenInstanceError):
-            setattr(iv, stat, 32)
-
-
-@pytest.mark.skip
-def test_single_ev_cannot_exceed_255():
-    with pytest.raises(MaxStatExceededError):
-        Stats(hp=256)
-
-
-@pytest.mark.skip
-def test_total_ev_cannot_exceed_510():
-    with pytest.raises(MaxStatExceededError):
-        Stats(hp=255, attack=255, defense=255)
