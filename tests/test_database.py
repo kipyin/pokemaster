3f47187177aad363938fb42ac454ea109ed8ae2e
Kip Yin <kipyyin@gmail.com>
2019-02-02 18:50:59 +0800

    Use expected == actual convention for assertions

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,7 @@
+"""
+Tests for `pokemaster.database`.
+"""
+
 from pokemaster import database
 
 
@@ -11,44 +15,48 @@ def test_bind_session():
 def test_get_machine():
     """Sanity test for getting the correct machine in Emerald."""
     focus_punch = database.get_machine(1)
-    assert focus_punch.move_id == 264
+    assert 264 == focus_punch.move_id
     dive = database.get_machine(108)
-    assert dive.move_id == 291
+    assert 291 == dive.move_id
     assert dive.is_hm is True
 
 
 def test_get_move_pool():
     assert database.get_move_pool(species='eevee', move_method='level-up')
-    assert len(database.get_move_pool(species='eevee')) != 0
+    assert 0 != len(database.get_move_pool(species='eevee'))
 
 
 def test_get_pokemon_by_national_id():
     """Given the national ID, a single Pokémon should be returned."""
     bulbasaur = database.get_pokemon(national_id=1)
-    assert bulbasaur.identifier == 'bulbasaur'
+    assert 'bulbasaur' == bulbasaur.identifier
 
 
 def test_get_pokemon_with_different_forms_by_national_id_():
     """Deoxys has 3 forms. `get_pokemon` should only return the default
-    form.
+    form, if no form is specified.
     """
     deoxys_normal = database.get_pokemon(national_id=386)
-    assert deoxys_normal.identifier == 'deoxys-normal'
+    assert 'deoxys-normal' == deoxys_normal.identifier
 
 
 def test_get_pokemon_by_species():
     """Get a Pokémon by its species."""
     bulbasaur = database.get_pokemon(species='bulbasaur')
-    assert bulbasaur.identifier == 'bulbasaur'
+    assert 'bulbasaur' == bulbasaur.identifier
 
 
 def test_get_pokemon_by_national_id_and_form():
     """Get a Pokémon by its National Pokédex ID and the form."""
     unown_x = database.get_pokemon(national_id=201, form='x')
-    assert unown_x.identifier == 'unown'
+    assert 'unown' == unown_x.identifier
 
 
 def test_get_pokemon_by_species_and_form():
     """Get a Pokémon by its species and its form."""
     castform_rainy = database.get_pokemon(species='castform', form='rainy')
-    assert castform_rainy.identifier == 'castform-rainy'
+    assert 'castform-rainy' == castform_rainy.identifier
+
+
+def test_get_exp_by_level():
+    """"""
