112646a121f10341a4ea824c762c1847c3880a8a
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-02-03 02:32:15 +0800

    Add the ability to generate HTML coverage report

--- tasks.py
+++ tasks.py
@@ -1,16 +1,37 @@
+"""
+Provides short-hand commands to aid testing and developing.
+"""
+
 from invoke import task
 
 
 @task
-def format(c):
+def lint(c):
+    """Lint the codes."""
     c.run('isort -rc .')
     c.run('black .')
 
 
 @task(iterable=['tests'])
-def test(c, tests):
+def test(c, tests=None, report=False):
+    """Run tests.
+
+    :param tests: The tests to run. This is a list of test file names
+        without the 'tests/' prefix. If no tests are given, all tests
+        will be run.
+    :param report: Generate the coverage report in HTML.
+    :return: NoReturn
+    """
     if not tests:
-        tests = ['tests/test_pokemon.py']
-    elif tests == 'all':
-        tests = ['tests/']
-    c.run(f'poetry run pytest -qx --ff {" ".join(tests)}')
+        tests_to_run = ['tests/']
+    else:
+        tests_to_run = list(map(lambda x: 'tests/' + x, tests))
+
+    c.run(
+        f'poetry run pytest '
+        f'-q --cov=pokemaster '
+        f'{"--cov-report=html" if report else ""} '
+        f'{" ".join(tests_to_run)}'
+    )
+    if report:
+        c.run('open htmlcov/index.html')
