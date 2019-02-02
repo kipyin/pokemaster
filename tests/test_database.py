38a83fc4cc2413a47607f9b3635dc2435e0014d8
Kip Yin <kipyyin@gmail.com>
2019-02-02 19:22:20 +0800

    Write more descriptive docstrings for tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,7 +13,8 @@ def test_bind_session():
 
 
 def test_get_machine():
-    """Sanity test for getting the correct machine in Emerald."""
+    """
+    """
     focus_punch = database.get_machine(1)
     assert 264 == focus_punch.move_id
     dive = database.get_machine(108)
@@ -22,41 +23,45 @@ def test_get_machine():
 
 
 def test_get_move_pool():
+    """
+    """
     assert database.get_move_pool(species='eevee', move_method='level-up')
     assert 0 != len(database.get_move_pool(species='eevee'))
 
 
 def test_get_pokemon_by_national_id():
-    """Given the national ID, a single Pokémon should be returned."""
+    """
+    A Pokémon's National Pokédex ID alone is enough to determine the
+    exact Pokémon we want to get.
+
+    Note that there is only one form for Bulbasaur.
+    """
     bulbasaur = database.get_pokemon(national_id=1)
     assert 'bulbasaur' == bulbasaur.identifier
 
 
-def test_get_pokemon_with_different_forms_by_national_id_():
-    """Deoxys has 3 forms. `get_pokemon` should only return the default
-    form, if no form is specified.
+def test_get_pokemon_by_species():
+    """
+    A Pokémon's speices (kind) alone is enough to determine the exact
+    Pokémon we want to get.
+    """
+    bulbasaur = database.get_pokemon(species='bulbasaur')
+    assert 'bulbasaur' == bulbasaur.identifier
+
+
+def test_get_default_pokemon_form():
+    """
+    The default form of a Pokémon is returned if the Pokémon has
+    multiple forms.
     """
     deoxys_normal = database.get_pokemon(national_id=386)
     assert 'deoxys-normal' == deoxys_normal.identifier
 
 
-def test_get_pokemon_by_species():
-    """Get a Pokémon by its species."""
-    bulbasaur = database.get_pokemon(species='bulbasaur')
-    assert 'bulbasaur' == bulbasaur.identifier
-
-
-def test_get_pokemon_by_national_id_and_form():
-    """Get a Pokémon by its National Pokédex ID and the form."""
-    unown_x = database.get_pokemon(national_id=201, form='x')
-    assert 'unown' == unown_x.identifier
-
-
-def test_get_pokemon_by_species_and_form():
-    """Get a Pokémon by its species and its form."""
+def test_get_specific_pokemon_form():
+    """
+    A Pokémon's species and a form name is sufficient to get the exact
+    Pokémon form for those who have multiple forms.
+    """
     castform_rainy = database.get_pokemon(species='castform', form='rainy')
     assert 'castform-rainy' == castform_rainy.identifier
-
-
-def test_get_exp_by_level():
-    """"""
