f47f8e8ea0589c954503afe14b1e8f8c41ed03d8
Kip Yin <28321392+kipyin@users.noreply.github.com>
2019-02-20 00:13:29 +0800

    Add tests for `PRNG.random()`

--- tests/test_prng.py
+++ tests/test_prng.py
@@ -4,6 +4,8 @@ Given a seed, the PRNG should have the exact same behavior.
 The seed and the results are from the following link:
 https://www.smogon.com/ingame/rng/pid_iv_creation#pokemon_random_number_generator
 """
+import pytest
+
 from pokemaster.prng import PRNG
 
 
@@ -14,7 +16,8 @@ def test_prng_default_seed_is_0():
 
 def test_next_5():
     prng = PRNG(0x1A56B091)
-    assert prng.next(5) == [0x01DB, 0x7B06, 0x5233, 0xE470, 0x5CC4]
+    assert prng.next(4) == [0x01DB, 0x7B06, 0x5233, 0xE470]
+    assert prng.next() == 0x5CC4
 
 
 def test_reset_prng():
@@ -28,3 +31,17 @@ def test_reset_prng():
 def test_pid_ivs_creation():
     prng = PRNG(0x560B9CE3)
     assert (0x7E482751, 0x5EE9629C) == prng.create_genome(method=2)
+
+
+def test_random():
+    prng = PRNG(0x1A56B091)
+    with pytest.raises(TypeError):
+        prng.random("10")
+    with pytest.raises(TypeError):
+        prng.random(min=10, max="20")
+
+    assert prng.random() == 0x01DB / 0x10000
+    assert prng.random(10) == 10 * 0x7B06 / 0x10000
+    assert prng.random(5, 10) == 5 * 0x5233 / 0x10000 + 5
+    with pytest.raises(ValueError):
+        prng.random(10, 5)
