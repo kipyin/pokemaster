a27433736f3b99dd139130e0cd817dc012186063
Kip Yin <28321392+kipyin@users.noreply.github.com>
2020-01-16 09:22:08 +0800

    Use pyproject.toml to store isort configs

--- pokemaster/__init__.py
+++ pokemaster/__init__.py
@@ -1,7 +1,7 @@
 """Public APIs."""
 from .__version__ import __version__
+from .game_version import Game
 from .pokemon import Pokemon
 from .weather import Weather
-from .game_version import Game
 
 __author__ = 'Kip Yin'
